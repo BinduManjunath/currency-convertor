6fe0fdf66a3fea598eb5de21da9e781199c3db2f
Bindu-Manjunath <44677458+Bindu-Manjunath@users.noreply.github.com>
2020-08-19 00:38:59 +0530

    Fetching the data from exchange api and displaying the graph

--- src/app/shared/components/currency-trends/currency-trends.component.ts
+++ src/app/shared/components/currency-trends/currency-trends.component.ts
@@ -19,14 +19,36 @@ export class CurrencyTrendsComponent implements OnInit {
   lineChartLegend = true;
   lineChartPlugins = [];
   lineChartType = 'line';
+  lineChartLabels: Label[]
   
   constructor() {
-    console.log(this.months)
-    this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    this.values = [0.0113403757, 0.0114006886, 0.0117741252, 0.0128303, 0.0121577591, 0.0120694962, 0.012670979, 0.0126312067, 0.0127462415, 0.0127532313, 0.0126127262, 0.0118891927]
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+  // CHART CLICK EVENT.
+  onChartClick(event) {
+    console.log(event);
+  }
+  ngOnInit() {
+    // console.log(this.months)
+    // this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+    // 'October', 'November', 'December'];
+    // this.values = [0.0113403757, 0.0114006886, 0.0117741252, 0.0128303,
+    //   0.0121577591, 0.0120694962, 0.012670979, 0.0126312067, 0.0127462415, 0.0127532313, 0.0126127262, 0.0118891927]
     this.lineChartData = [
       { data: this.values, label: 'Trend in INR against ' + this.baseCurrency },
     ];
+    this.lineChartLabels = this.months;
 
     this.lineChartOptions = {
       responsive: true,
@@ -38,23 +60,7 @@ export class CurrencyTrendsComponent implements OnInit {
         backgroundColor: 'rgba(255,255,0,0.28)',
       },
     ];
-  }
-
-
-
-
-
-
-
-
-
-
-
-  // CHART CLICK EVENT.
-  onChartClick(event) {
-    console.log(event);
-  }
-  ngOnInit() {
+  
   }
 
 }
